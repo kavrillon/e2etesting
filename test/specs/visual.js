79dea51e697ce8b01f294f0b924756ffc9e3e3df
Kevin Avrillon <kavrillon@leadformance.com>
2017-03-09 14:37:04 +0100

    test(app): add visual reference

--- test/specs/visual.js
+++ test/specs/visual.js
@@ -1,13 +1,15 @@
 let assert = require('assert');
 
 describe('visual regressions checker', () => {
+    const url = '/?' + Math.random(); // add some randomness for cache
+
     it('should be the same body', () => {
-        browser.url('/').pause(1000);
+        browser.url(url).pause(1000);
         assert.ok(browser.checkDocument()[0].isWithinMisMatchTolerance);
     });
 
     it('should be the same header', () => {
-        browser.url('/').pause(1000);
+        browser.url(url).pause(1000);
 
         assert.ok(browser.checkElement('header')[0].isWithinMisMatchTolerance);
     });
