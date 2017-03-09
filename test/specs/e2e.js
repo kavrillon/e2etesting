ee9f53eff2eda370a7ca563b14f29cfe47fba762
Kevin Avrillon <kavrillon@leadformance.com>
2017-03-09 11:56:31 +0100

    feat(tests): init visual testing

--- test/specs/e2e.js
+++ test/specs/e2e.js
@@ -1,10 +1,12 @@
 let assert = require('assert');
+import pjson from '../../package.json';
+
 
 describe('e2e checker', () => {
 
     it('should be the correct title', () => {
         browser.url('/');
         let title = browser.getTitle();
-        assert.equal(title, 'FO Tester');
+        assert.equal(title, pjson.name);
     });
 });
