fce09d0b4609c9f725690f2036b27c1bec011520
Kevin Avrillon <kavrillon@leadformance.com>
2017-03-14 12:41:48 +0100

    feat(app): add W3C validator

--- test/specs/e2e.js
+++ test/specs/e2e.js
@@ -1,25 +1,29 @@
 var w3cjs = require('w3cjs');
+require('jasmine2-custom-message');
 const url = '/?' + Math.random(); // add some randomness for cache
 
 describe('Technical', () => {
+    beforeAll(function (done) {
+        browser.url(url).call(done);
+    });
+
+    afterAll(function (done) {
+        browser.end(done);
+    });
+
     it('should be a title', () => {
-        browser.url(url);
         expect(browser.getTitle()).not.toBe('');
     });
 
     it('should be a description', () => {
-        browser.url(url);
         expect(browser.getAttribute('meta[name="description"]', 'content')).not.toBe('');
     });
 
     it('should have one <h1>', () => {
-        browser.url(url);
         expect(browser.elements('h1').value.length).toEqual(1);
     });
 
-    it('should have no hidden title (h levels)', () => {
-        browser.url(url);
-
+    it('should have no hidden heading (h levels)', () => {
         const displays = browser.getCssProperty('h1,h2,h3,h4,h5,h6', 'display');
         const visibilities = browser.getCssProperty('h1,h2,h3,h4,h5,h6', 'visibility');
         const opacities = browser.getCssProperty('h1,h2,h3,h4,h5,h6', 'opacity');
@@ -44,10 +48,9 @@ describe('Technical', () => {
         expect(isHiddenTitle).toBe(false);
     });
 
-    it('should have no hierarchy break (h levels)', () => {
-        browser.url(url);
-
+    it('should have no hierarchy break in headings (h levels)', () => {
         let isValid = true;
+        let logMessage = '';
         let headings = browser.getTagName('h1,h2,h3,h4,h5,h6');
         let level = 0;
 
@@ -55,34 +58,40 @@ describe('Technical', () => {
             const match = elt.match(/[0-9]/);
             if (match) {
                 const val = match[0];
+
+                // check if diff between previous header level & current one is greater than one
+                // if yes => there is a hierarchy break
                 if (val - level > 1) {
                     isValid = false;
+                    logMessage = `A <h${val}> is present without <h${val-1}>`;
                 }
                 level = val;
             }
         });
 
-        expect(isValid).toBe(true);
+        since(logMessage).expect(isValid).toBe(true);
     });
 
     it('should have no w3c errors', () => {
-        browser.url(url);
-
         const html = browser.getSource();
+        let logMessage = '';
 
-        w3cjs.validate({
-            input: html,
-            callback: function (res) {
-                expect(res.messages.length).toBe(0);
-                // done();
-                // res.messages.forEach((msg) => {
-                //     console.log(msg.type + ' - ' + msg.message + ' (' + msg.lastLine + ')');
-                //     // fail(msg);
-                // });
-                // if (res.messages.length > 0 ) {
-                //     throw {error: 'html errors have been found', results: res};
-                // }
-            }
+        return new Promise((resolve) => {
+            w3cjs.validate({
+                input: html,
+                callback: function (res) {
+                    resolve(res.messages);
+                }
+            });
+        }).then((messages) => {
+            let countErrors = 0;
+            messages.forEach((msg) => {
+                logMessage += '\n' + msg.type + ' (' + msg.lastLine + ')' + ': ' + msg.message;
+                if (msg.type === 'error') {
+                    countErrors++;
+                }
+            });
+            since(logMessage).expect(countErrors).toBe(0);
         });
     });
 });
@@ -96,8 +105,16 @@ describe('Technical', () => {
 // });
 
 describe('Visual Regressions', () => {
+    beforeAll(function (done) {
+        browser.url(url).call(done);
+    });
+
+    afterAll(function (done) {
+        browser.end(done);
+    });
+
     it('should be the same body', () => {
-        browser.url(url).pause(1000);
+        browser.pause(1000);
         expect(browser.checkDocument()[0].isWithinMisMatchTolerance).toBe(true);
     });
 });
